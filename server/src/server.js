a15f5e535b339cea15f0bc4899f5c100b04662bf
anshul-999 <anshul.b100@gmail.com>
2026-04-30 12:19:15 +0530

    redo servre.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,49 +1,30 @@
-// import app from "./index.js";
-// import db from "./config/db.js";
-// import dotenv from "dotenv";
+import app from "./index.js";
+import db from "./config/db.js";
+import dotenv from "dotenv";
 
-// dotenv.config();
-
-// const PORT = process.env.PORT || 5000;
-
-// app.get("/", (req, res) => {
-//   res.send("API is running. Use /api routes to interact with the backend.");
-// });
-
-// app.get("/health", (req, res) => {
-//   res.json({
-//     status: "OK",
-//     message: "Server is up and running",
-//   });
-// });
-
-// db()
-//   .then(() => {
-//     console.log("MongoDB connected");
-
-//     app.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("DB connection failed:", error);
-//   });
-
-import express from "express";
-
-console.log("ENV CHECK:");
-console.log("MONGODB_STR:", process.env.MONGODB_STR);
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
-console.log("PORT:", process.env.PORT);
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("SERVER RUNNING");
-});
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log("Server started on", PORT);
+app.get("/", (req, res) => {
+  res.send("API is running. Use /api routes to interact with the backend.");
 });
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "Server is up and running",
+  });
+});
+
+db()
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("DB connection failed:", error);
+  });
